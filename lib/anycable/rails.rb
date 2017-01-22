d51a7ef90babf34d384bdd90d2353cb474d0a0e7
palkan <dementiev.vm@gmail.com>
2017-01-22 20:37:42 +0300

    Fix server script

--- lib/anycable/rails.rb
+++ lib/anycable/rails.rb
@@ -4,6 +4,8 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require "anycable"
 require "anycable/rails/version"
 require "anycable/rails/actioncable/server"
+require "anycable/rails/actioncable/connection"
+
 module Anycable
   # Rails handler for AnyCable
   module Rails
--- lib/anycable/rails/version.rb
+++ lib/anycable/rails/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Anycable
   module Rails
-    VERSION = "0.4.0"
+    VERSION = "0.4.1"
   end
 end
