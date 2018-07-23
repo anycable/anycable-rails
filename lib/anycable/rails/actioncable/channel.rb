9f0cbe4359a83c36740bd02d21733895d5b65cbd
Andrey Novikov <envek@envek.name>
2018-07-23 17:41:31 +0300

    Fix raising exception about unsupported stream callbacks
    Exception classes are not methods.

--- lib/anycable/rails/actioncable/channel.rb
+++ lib/anycable/rails/actioncable/channel.rb
@@ -14,7 +14,9 @@ module ActionCable
       end
 
       def stream_from(broadcasting, callback = nil, coder: nil)
-        raise ArgumentError('Unsupported') if callback.present? || coder.present? || block_given?
+        if callback.present? || coder.present? || block_given?
+          raise ArgumentError, 'Custom stream callbacks are not supported in AnyCable!'
+        end
         connection.socket.subscribe identifier, broadcasting
       end
 
