c313da326f0d7813a410ffcd0440973b5e0603e4
Vladimir Dementyev <dementiev.vm@gmail.com>
2018-09-18 14:07:42 -0400

    Fix rubocop offenses

--- lib/anycable/rails/actioncable/channel.rb
+++ lib/anycable/rails/actioncable/channel.rb
@@ -17,6 +17,7 @@ module ActionCable
         if callback.present? || coder.present? || block_given?
           raise ArgumentError, 'Custom stream callbacks are not supported in AnyCable!'
         end
+
         connection.socket.subscribe identifier, broadcasting
       end
 
