f2846a4a58fa434dce1ef8d0e2e2c07fedd583c2
DmitryTsepelev <dmitry.a.tsepelev@gmail.com>
2018-11-02 17:17:23 +0300

    Better detection of instance variables inside actions
    (cherry picked from commit 8a836019bdcce94420e2ab1ad4898236886c04f9)

--- spec/rubocop/instance_vars_spec.rb
+++ spec/rubocop/instance_vars_spec.rb
@@ -5,36 +5,7 @@ require "spec_helper"
 RSpec.describe Anycable::Compatibility::Anycable::InstanceVars do
   include_context "cop spec"
 
-  it "registers offense for instance vars in the middle of #subscribed" do
-    inspect_source(<<-RUBY.strip_indent)
-      class MyChannel < ApplicationCable::Channel
-        def subscribed
-          method_call
-          @instance_var = 'something'
-          stream_from @instance_var
-        end
-      end
-    RUBY
-
-    expect(cop.offenses.size).to be(1)
-    expect(cop.messages.first).to eq("Subscription instance variables are not supported in AnyCable")
-  end
-
-  it "registers offense for instance vars in the beginning of #subscribed" do
-    inspect_source(<<-RUBY.strip_indent)
-      class MyChannel < ApplicationCable::Channel
-        def subscribed
-          @instance_var = 'something'
-          stream_from @instance_var
-        end
-      end
-    RUBY
-
-    expect(cop.offenses.size).to be(1)
-    expect(cop.messages.first).to eq("Subscription instance variables are not supported in AnyCable")
-  end
-
-  it "registers offense for instance var definitions in #subscribed" do
+  it 'registers offense for instance var declaration in #subscribed' do
     inspect_source(<<-RUBY.strip_indent)
       class MyChannel < ApplicationCable::Channel
         def subscribed
@@ -47,11 +18,24 @@ RSpec.describe Anycable::Compatibility::Anycable::InstanceVars do
     expect(cop.messages.first).to eq("Subscription instance variables are not supported in AnyCable")
   end
 
-  it "registers offense for instance var definitions inside block in #subscribed" do
+  it 'registers offense for instance var definition inside block in #subscribed' do
     inspect_source(<<-RUBY.strip_indent)
       class MyChannel < ApplicationCable::Channel
         def subscribed
           5.times { @instance_var = 1 }
+        end
+      end
+    RUBY
+
+    expect(cop.offenses.size).to be(1)
+    expect(cop.messages.first).to eq("Subscription instance variables are not supported in AnyCable")
+  end
+
+  it 'registers offense for instance var definitions inside action' do
+    inspect_source(<<-RUBY.strip_indent)
+      class MyChannel < ApplicationCable::Channel
+        def follow
+          @instance_var = 1
         end
       end
     RUBY
