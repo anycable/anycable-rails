2381b9e1134b952b863e052b64af8fe6f5680c26
Sebastian A. Espindola <sespindola@gmail.com>
2019-01-10 12:16:54 -0300

    fixed anycable config dependency (#68)
    * fixed anycable config dependency
    
    * fixed rubocop offense

--- lib/anycable/rails/config.rb
+++ lib/anycable/rails/config.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "anycable/config"
+
 # Extend AnyCable configuration with
 # `access_logs_disabled` options (defaults to true)
 AnyCable::Config.attr_config access_logs_disabled: true
