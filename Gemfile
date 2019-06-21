651a165b8f0cdd59b3b78d2a25cd5570553d6f65
Vladimir Dementyev <dementiev.vm@gmail.com>
2019-06-21 00:04:38 -0400

    specs: upd logging context for Rails 6
    Tagged logger return a new logger instance now: https://github.com/rails/rails/pull/27792

--- Gemfile
+++ Gemfile
@@ -2,10 +2,11 @@ source 'https://rubygems.org'
 
 gemspec
 
-gem 'sqlite3', '~> 1.3'
-
 local_gemfile = "#{File.dirname(__FILE__)}/Gemfile.local"
 
 if File.exist?(local_gemfile)
   eval(File.read(local_gemfile)) # rubocop:disable Lint/Eval
+else
+  gem 'sqlite3', '~> 1.3'
+  gem 'rails', '~> 5.2'
 end
--- spec/support/logging_context.rb
+++ spec/support/logging_context.rb
@@ -3,8 +3,8 @@
 shared_context "log:info", log: :info do
   before do
     @old_level = Rails.logger.level
-    Rails.logger.level = :info
+    Rails.logger.level = AnyCable.logger.level = :info
   end
 
-  after { Rails.logger.level = @old_level }
+  after { Rails.logger.level = AnyCable.logger.level = @old_level }
 end
