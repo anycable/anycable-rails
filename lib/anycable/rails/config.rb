5a80d2225f54176f0c4d463efb181d40f802b300
Vladimir Dementyev <dementiev.vm@gmail.com>
2021-05-12 21:53:56 +0300

    fix: ensure anyway/rails is loaded

--- lib/anycable/rails/config.rb
+++ lib/anycable/rails/config.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require "anycable/config"
+# Make sure Rails extensions for Anyway Config are loaded
+# See https://github.com/anycable/anycable-rails/issues/63
+require "anyway/rails"
 
 # Extend AnyCable configuration with:
 # - `access_logs_disabled` (defaults to true) — whether to print Started/Finished logs
