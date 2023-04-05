da8990517630354a0bc84f33dab0985e669f80b5
Vladimir Dementyev <palkan@hey.com>
2023-04-05 10:41:36 -0400

    dev: configure coveralls

--- spec/base_spec_helper.rb
+++ spec/base_spec_helper.rb
@@ -7,6 +7,26 @@ begin
 rescue LoadError, NoMethodError
 end
 
+if ENV["COVERAGE"] == "true"
+  require "simplecov"
+  SimpleCov.start do
+    enable_coverage :branch
+
+    add_filter "/spec/"
+  end
+
+  require "simplecov-lcov"
+  SimpleCov::Formatter::LcovFormatter.config do |c|
+    c.report_with_single_file = true
+    c.single_report_path = "coverage/lcov.info"
+  end
+
+  SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::LcovFormatter
+  ])
+end
+
 PROJECT_ROOT = File.expand_path("../", __dir__)
 
 RSpec.configure do |config|
