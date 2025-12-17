84f795c0a196190f02f0649c7ebdeb339969ecba
Vladimir Dementyev <dementiev.vm@gmail.com>
2025-12-18 00:46:00 +0300

    * upgrade rubocop-md

--- spec/cops_spec_helper.rb
+++ spec/cops_spec_helper.rb
@@ -9,9 +9,6 @@ require "rubocop/rspec/support"
 shared_context "cop spec" do
   include RuboCop::RSpec::ExpectOffense
 
-  # Make config explicit (Rails 6.0 specs failed due to #config method defined)
-  let(:config) { RuboCop::Config.new({}, "#{Dir.pwd}/.rubocop.yml") }
-
   subject(:cop) { described_class.new }
 
   it "works with empty file" do
